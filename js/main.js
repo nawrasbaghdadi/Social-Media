4152b0c678b7b31448f7cb92c898cd43bd11d4a7
nawras <nawras.mbc@gmail.com>
2014-05-01 17:51:26 +0400

    working on the posting menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
+	function active (selector) {
+		$(selector).on('click',function (e){
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$(this).siblings().removeClass('active');
+	})
+	}
 
+	active('.accounts > ul > li');
+	active('.acc-groups .thumbnail a');
+	$('.create-post').on('click',function (e){
+		e.preventDefault();
+		$('.post-panel').toggleClass('panel-active');
+		$(this).toggleClass('active');
+	})
 	
-
+    $('#layout-changer a').click(function (e) {
+     e.preventDefault()
+      $(this).tab('show')
+     })
 
 
 	// body...
