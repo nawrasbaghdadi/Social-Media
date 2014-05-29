1c5f1062edc586de11bfd3db0e2d4136abf023f5
freesoft <freesoft@freesofts-MacBook-Pro.local>
2014-05-29 15:17:09 +0400

    finish choseing the accounts

--- js/main.js
+++ js/main.js
@@ -8,16 +8,38 @@ $(document).ready(function () {
 	}
 
 	active('.accounts > ul > li');
-	active('.acc-groups .thumbnail a');
+	//active('.acc-groups .thumbnail');
+	function toggel (selector) {
+		$(this).toggleClass('active');
+		$el = $(this).clone();
+		if($(this).hasClass('active')){
+			$('.selected-accounts ul').append($el);	
+		}else{
+			console.log($el);
+			($el).remove();	
+		}
+	}
+	//$('.acc-groups .thumbnail')[0].addEventListener('click' , toggel , false);
+	$('.acc-groups .thumbnail').on('click',function (e){
+		 $(this).toggleClass('active',10).promise().done(function() {
+		 	$el = $(this).clone()[0];
+		 	id=$(this).attr('id');
+			if($(this).hasClass('active')){
+			$('.selected-accounts ul').append($el);			
+			}else{
+			$('#'+id).remove();	
+			}
+		});
+		
+	})
 	$('.create-post').on('click',function (e){
 		e.preventDefault();
 		$('.post-panel').toggleClass('panel-active');
-		$(this).toggleClass('active');
 	})
 	
     $('#layout-changer a').click(function (e) {
      e.preventDefault()
-      $(this).tab('show')
+      $(this).tab('show');
      })
 
 
