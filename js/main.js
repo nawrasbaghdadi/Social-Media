725428021229e6e70dd35adf3e6afa43782d2d5f
System Administrator <root@freesofts-MacBook-Pro.local>
2014-06-01 21:31:16 +0400

    add autocomplete for the articles and input file skin

--- js/main.js
+++ js/main.js
@@ -34,6 +34,10 @@ $(document).ready(function () {
 		e.preventDefault();
 		$('.post-panel').toggleClass('panel-active');
 	})
+	$('#add-article').on('click',function (e){
+		e.preventDefault();
+		$('.articles').toggleClass('articles-active');
+	})
 	$('body').on('click', '.selected-accounts .thumbnail' ,function (e) {
 		e.preventDefault();
 		$(this).remove();
@@ -42,6 +46,9 @@ $(document).ready(function () {
      e.preventDefault()
       $(this).tab('show');
      })
+/////////Input File////////////    
+    $('input[type=file]').bootstrapFileInput();
+/////////Input File////////////
    
 })
 
@@ -68,4 +75,36 @@ $(document).ready(function () {
         $(".counted").limiter(300, elem);
 
 ////////
+ $.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _renderMenu: function( ul, items ) {
+      var that = this,
+        currentCategory = "";
+      $.each( items, function( index, item ) {
+        if ( item.category != currentCategory ) {
+          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+          currentCategory = item.category;
+        }
+        that._renderItemData( ul, item );
+      });
+    }
+  });
+  $(function() {
+    var data = [
+      { label: "anders", category: "" },
+      { label: "andreas", category: "" },
+      { label: "antal", category: "" },
+      { label: "annhhx10", category: "Products" },
+      { label: "annk K12", category: "Products" },
+      { label: "annttop C13", category: "Products" },
+      { label: "anders andersson", category: "People" },
+      { label: "andreas andersson", category: "People" },
+      { label: "andreas johnson", category: "People" }
+    ];
+ 
+    $( "#search" ).catcomplete({
+      delay: 0,
+      source: data
+    });
+  });
+
 	
